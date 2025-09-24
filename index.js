5540da176aa5f9d3fe10e2fc4c6cab73c721f69c
Cat <141873540+DTACat@users.noreply.github.com>
2025-09-24 18:24:00 +1200

    f

---
+++ index.js
@@ -0,0 +1,5 @@
+const css = document.createElement('link');
+css.href = 'https://raw.githubusercontent.com/DTACat/Discord-Profiles-Plus/version3/index.css';
+css.type = 'text/css';
+css.rel  = 'stylesheet';
+document.body.appendChild(css);
